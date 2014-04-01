7d04d22062051cd79960bb4305c38d5691f21d1d
lledoyen <ledoyen.loic@gmail.com>
2014-04-01 15:18:53 +0200

    draft of Forward view

---
+++ extended-aash-http-server/src/main/scala/com/ledoyen/scala/aash/httpserver/extended/ForwardView.scala
@@ -0,0 +1,51 @@
+package com.ledoyen.scala.aash.httpserver.extended
+
+import java.net.URL
+import com.ledoyen.scala.aash.httpserver._
+import org.slf4j.LoggerFactory
+import com.ledoyen.scala.aash.tool.AsyncHttp
+
+object ForwardView {
+  def logger = LoggerFactory.getLogger("ForwardView")
+
+  def remainingPath(req: HttpRequest): String = req.path.substring(req.listenerPath.length)
+
+  def replaceUrls(listenerPath: String)(body: String): String = {
+    body
+      .replaceAllLiterally("href=\"/", "href=\"" + listenerPath + "/")
+      .replaceAllLiterally("src=\"/", "href=\"" + listenerPath + "/")
+      // replace all nor relatives
+      .replaceAllLiterally("href=\"" + listenerPath + "//", "href=\"//")
+      .replaceAllLiterally("src=\"" + listenerPath + "//", "href=\"//")
+  }
+}
+
+class ForwardView(val rootUrl: URL) extends (HttpRequest => HttpResponse) {
+  import java.net.HttpURLConnection
+  import java.net.MalformedURLException
+
+  def apply(req: HttpRequest): HttpResponse = {
+    Http.connect(rootUrl, req.withPath(ForwardView.remainingPath(req))) match {
+      case Some(resp) => {
+        val r = resp.changeBody(ForwardView.replaceUrls(req.listenerPath))
+        ForwardView.logger.trace(r.toString)
+        r
+      }
+      case None => Http.error(new RuntimeException("Unknown issue"))
+    }
+  }
+}
+
+class AsyncForwardView(val rootUrl: URL) extends ((HttpRequest, WriteCallback) => Unit) {
+
+  def apply(req: HttpRequest, callback: WriteCallback) = {
+    AsyncHttp.connect(rootUrl, req.withPath(ForwardView.remainingPath(req)), true, maybe => maybe match {
+      case Right(resp) => {
+        val r = resp.changeBody(ForwardView.replaceUrls(req.listenerPath))
+        ForwardView.logger.trace(r.toString)
+        callback.write(r)
+      }
+      case Left(t) => callback.write(Http.error(t))
+    })
+  }
+}
---
+++ extended-aash-http-server/src/test/scala/com/ledoyen/scala/aash/httpserver/extended/ForwardViewTest.scala
@@ -0,0 +1,57 @@
+package com.ledoyen.scala.aash.httpserver.extended
+
+import org.junit.Assert._
+import com.ledoyen.scala.aash.httpserver._
+import java.net.HttpURLConnection
+import java.net.URL
+import org.junit.Test
+import com.gargoylesoftware.htmlunit.WebClient
+import com.gargoylesoftware.htmlunit.html.HtmlPage
+import org.hamcrest.CoreMatchers
+import org.junit.Ignore
+
+class ForwardViewTest {
+
+  @Test
+//  @Ignore
+  def testForward() {
+    val realServer = HttpServer.sync(8060).start
+    realServer.registerListener("/real", req => HttpResponse(req.version, StatusCode.OK, "<html><head><title>Hey hey !</title></head><body>toto !</body></html>", Map("Content-Type" -> "text/html")))
+    val proxyServer = HttpServer.sync(8070).start
+    proxyServer.registerListener("/toto", new ForwardView(new URL("http://localhost:8060/real")))
+    val webClient = new WebClient
+    //    Thread.sleep(1000)
+    val realPage: HtmlPage = webClient.getPage("http://localhost:8060/real")
+    val proxyPage: HtmlPage = webClient.getPage("http://localhost:8070/toto")
+
+    assertThat(proxyPage.getTitleText, CoreMatchers.equalTo(realPage.getTitleText))
+
+    realServer.stop
+    proxyServer.stop
+  }
+
+  @Test
+//  @Ignore
+  def testAsyncForward() {
+    val realServer = HttpServer.sync(8060).start
+    realServer.registerListener("/real", req => HttpResponse(req.version, StatusCode.OK, "<html><head><title>Hey hey !</title></head><body>toto !</body></html>", Map("Content-Type" -> "text/html")))
+    val proxyServer = HttpServer.async(8070).start
+    proxyServer.registerAsyncListener("/toto", new AsyncForwardView(new URL("http://localhost:8060/real")))
+    val webClient = new WebClient
+    //    Thread.sleep(1000)
+    val realPage: HtmlPage = webClient.getPage("http://localhost:8060/real")
+    val proxyPage: HtmlPage = webClient.getPage("http://localhost:8070/toto")
+
+    assertThat(proxyPage.getTitleText, CoreMatchers.equalTo(realPage.getTitleText))
+
+    realServer.stop
+    proxyServer.stop
+  }
+}
+
+object ForwardViewTest {
+  def main(args: Array[String]) {
+    val server = new SyncHttpServer(8090).start
+    server.registerListener("/toto", new ForwardView(new URL("http://www.totoofficial.com/"))) // OK
+  }
+}
